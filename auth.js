cdbae6ff47f9a1b0e9e5f955f457581bf875d6ce
bsepehri-hue <bsepehri@gmail.com>
2025-10-24 15:17:26 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,30 +1,26 @@
 // Firebase v8 Auth Logic
 var firebaseConfig = {
-  apiKey: "AIzaSyC1Tundh0nE4_5E3uH3pL5eUchghg",
-  authDomain: "listtobid-3ecae.firebaseapp.com",
-  projectId: "listtobid-3ecae",
-  storageBucket: "listtobid-3ecae.appspot.com",
-  messagingSenderId: "188620290063",
-  appId: "1:188620290063:web:3e4f9f1e9e2e9e9e9e9e9e"
+  apiKey: "AIzaSyCTtUnnoNdEA_SE4u3pSbLsehgHC",
+  authDomain: "listoid-b3eca.firebaseapp.com",
+  projectId: "listoid-b3eca",
+  storageBucket: "listoid-b3eca.appspot.com",
+  messagingSenderId: "188888888888",
+  appId: "1:188888888888:web:8888888888888888"
 };
 firebase.initializeApp(firebaseConfig);
 
-// Handle redirect result
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.user) {
-    alert("Signed in as: " + result.user.email);
-  }
-}).catch(function(error) {
-  alert("Redirect error: " + error.message);
-});
-
-// Attach click listener
 document.addEventListener("DOMContentLoaded", function () {
   var btn = document.getElementById("google-signin-button");
   if (btn) {
     btn.addEventListener("click", function () {
       var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().signInWithPopup(provider)
+        .then(function(result) {
+          alert("Signed in as: " + result.user.email);
+        })
+        .catch(function(error) {
+          alert("Sign-in error: " + error.message);
+        });
     });
   }
 });

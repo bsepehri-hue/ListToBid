04f95e868a69aa462cdd11c8c9cabe033331ad81
bsepehri-hue <bsepehri@gmail.com>
2025-10-24 15:23:00 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,26 +1,17 @@
-// Firebase v8 Auth Logic
-var firebaseConfig = {
-  apiKey: "AIzaSyCTtUnnoNdEA_SE4u3pSbLsehgHC",
-  authDomain: "listoid-b3eca.firebaseapp.com",
-  projectId: "listoid-b3eca",
-  storageBucket: "listoid-b3eca.appspot.com",
-  messagingSenderId: "188888888888",
-  appId: "1:188888888888:web:8888888888888888"
-};
-firebase.initializeApp(firebaseConfig);
-
 document.addEventListener("DOMContentLoaded", function () {
   var btn = document.getElementById("google-signin-button");
   if (btn) {
     btn.addEventListener("click", function () {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider)
-        .then(function(result) {
-          alert("Signed in as: " + result.user.email);
-        })
-        .catch(function(error) {
-          alert("Sign-in error: " + error.message);
-        });
+      setTimeout(function () {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+          .then(function(result) {
+            alert("Signed in as: " + result.user.email);
+          })
+          .catch(function(error) {
+            alert("Sign-in error: " + error.message);
+          });
+      }, 100); // 100ms delay
     });
   }
 });

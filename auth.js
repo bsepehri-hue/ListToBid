9b87e02e0c6b08edadd2c1fcbe2bfc9aea184623
bsepehri-hue <bsepehri@gmail.com>
2025-10-26 01:10:18 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
 
-// Firebase config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAS-eRT9CKzEvWuBS55Gqzaoa0lB1f_1lg",
+  authDomain: "oakportal-57694.firebaseapp.com",
+  projectId: "oakportal-57694",
+  storageBucket: "oakportal-57694.firebasestorage.app",
+  messagingSenderId: "102420516875",
+  appId: "1:102420516875:web:b8d86253b30a62e37f370e"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

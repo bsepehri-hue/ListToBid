ef604ce39f7fe4b008cc84e579b20bae4a41ad6e
bsepehri-hue <bsepehri@gmail.com>
2025-11-16 20:54:19 -0800

    Update pages.index.js

--- pages.index.js
+++ pages.index.js
@@ -1,12 +1,1 @@
-import Link from 'next/link';
-
-export default function Home() {
-  return (
-    <div className="parchment p-8">
-      <h1 className="text-4xl mb-6">Welcome to ListToBid</h1>
-      <Link href="/onboard"><button className="btn-emerald">Become a Steward</button></Link>
-      <Link href="/directory"><button className="btn-amber ml-4">Browse Merchants</button></Link>
-      <Link href="/vault"><button className="btn-gold ml-4">View Earnings</button></Link>
-    </div>
-  );
-}
+delete this file

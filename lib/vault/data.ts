e9eaab95ac68715c7bd5d1814318f641abfdf67a
bsepehri-hue <bsepehri@gmail.com>
2025-12-01 20:23:20 -0800

    Create data.ts

---
+++ lib/vault/data.ts
@@ -0,0 +1,7 @@
+export async function getVaultSummary(userId: string) {
+  // Stripe + Firestore logic here
+}
+
+export async function getTransactionLedger(userId: string) {
+  // Firestore query for steward’s transactions
+}

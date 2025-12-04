32f58028472f46a95a9ef423e7f168d1789aec6b
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 02:35:17 -0800

    Create auction/BiddingForm.tsx

---
+++ components/auction/BiddingForm.tsx
@@ -0,0 +1,8 @@
+export default function BiddingForm() {
+  return (
+    <form>
+      <input type="number" placeholder="Enter bid" />
+      <button type="submit">Place Bid</button>
+    </form>
+  );
+}

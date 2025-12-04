f624d955a7134068ec77e3f616850dcdd488f6e4
bsepehri-hue <bsepehri@gmail.com>
2025-12-03 17:57:51 -0800

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,16 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
+      apiKey: "AIzaSyCJYKumffrnbNU_4F3ItEU3aHLe8UuGhbg",
+      authDomain: "listtobid-9ede2.firebaseapp.com",
+      projectId: "listtobid-9ede2",
+      storageBucket: "listtobid-9ede2.appspot.com",
+      messagingSenderId: "482806996303",
+      appId: "1:482806996303:web:9093c9e0ca4c434a36f93a"
+    };
 
 export const app = initializeApp(firebaseConfig);
+
+
+
+    

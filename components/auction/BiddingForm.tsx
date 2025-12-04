ef90ccccfd5a3bfea458582c4cfc4314fe612263
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 13:40:39 -0800

    Update BiddingForm.tsx

--- components/auction/BiddingForm.tsx
+++ components/auction/BiddingForm.tsx
@@ -1,8 +1,20 @@
+// components/auction/BiddingForm.tsx
+"use client";
+
 export default function BiddingForm() {
   return (
-    <form>
-      <input type="number" placeholder="Enter bid" />
-      <button type="submit">Place Bid</button>
+    <form className="flex flex-col gap-2">
+      <input
+        type="number"
+        placeholder="Enter bid"
+        className="border rounded px-2 py-1"
+      />
+      <button
+        type="submit"
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+      >
+        Place Bid
+      </button>
     </form>
   );
 }

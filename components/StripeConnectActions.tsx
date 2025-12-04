942de422abc9ab07465e13bc77df0b891b75d37b
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 03:31:16 -0800

    Create StripeConnectActions.tsx

---
+++ components/StripeConnectActions.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function StripeConnectActions() {
+  return (
+    <div>
+      <h3>Stripe Connect Actions</h3>
+      <button>Connect Stripe</button>
+    </div>
+  );
+}

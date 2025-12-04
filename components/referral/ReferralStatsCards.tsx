ca80935678da41741325a8e6e0621db2106f622d
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 13:50:09 -0800

    Update ReferralStatsCards.tsx

--- components/referral/ReferralStatsCards.tsx
+++ components/referral/ReferralStatsCards.tsx
@@ -1,10 +1,11 @@
+// components/referral/ReferralStatsCards.tsx
 "use client";
 
 export default function ReferralStatsCards() {
   return (
-    <div>
-      <h3>Referral Stats</h3>
-      <ul>
+    <div className="p-4 border rounded">
+      <h3 className="font-semibold mb-2">Referral Stats</h3>
+      <ul className="space-y-1 text-gray-700">
         <li>Clicks: 0</li>
         <li>Signups: 0</li>
         <li>Earnings: $0</li>

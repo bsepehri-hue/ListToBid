9c47e2f02c673a20ae86f5743c6309f0054db425
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 03:15:46 -0800

    Create referral/ReferralStatsCards.tsx

---
+++ components/referral/ReferralStatsCards.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function ReferralStatsCards() {
+  return (
+    <div>
+      <h3>Referral Stats</h3>
+      <ul>
+        <li>Clicks: 0</li>
+        <li>Signups: 0</li>
+        <li>Earnings: $0</li>
+      </ul>
+    </div>
+  );
+}

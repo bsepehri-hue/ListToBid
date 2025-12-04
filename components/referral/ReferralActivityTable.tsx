fda505f6e9dfa448a05e643a7de73397242cb64f
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 13:52:03 -0800

    Update ReferralActivityTable.tsx

--- components/referral/ReferralActivityTable.tsx
+++ components/referral/ReferralActivityTable.tsx
@@ -1,22 +1,23 @@
+// components/referral/ReferralActivityTable.tsx
 "use client";
 
 export default function ReferralActivityTable() {
   return (
-    <div>
-      <h3>Referral Activity</h3>
-      <table>
+    <div className="p-4 border rounded">
+      <h3 className="font-semibold mb-2">Referral Activity</h3>
+      <table className="w-full border-collapse border text-sm">
         <thead>
-          <tr>
-            <th>Date</th>
-            <th>User</th>
-            <th>Action</th>
+          <tr className="bg-gray-100">
+            <th className="border px-2 py-1 text-left">Date</th>
+            <th className="border px-2 py-1 text-left">User</th>
+            <th className="border px-2 py-1 text-left">Action</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>No data</td>
-            <td>-</td>
-            <td>-</td>
+            <td className="border px-2 py-1">No data</td>
+            <td className="border px-2 py-1">-</td>
+            <td className="border px-2 py-1">-</td>
           </tr>
         </tbody>
       </table>

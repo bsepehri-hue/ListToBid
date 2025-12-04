92d45a6bee2a6234177cc8136e0d7c1239316e0a
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 03:24:18 -0800

    Create ReferralActivityTable.tsx

---
+++ components/referral/ReferralActivityTable.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+export default function ReferralActivityTable() {
+  return (
+    <div>
+      <h3>Referral Activity</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>User</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>No data</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}

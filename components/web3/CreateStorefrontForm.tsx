c06d80eb72596164d5fb0877e08f700411a86578
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 03:32:47 -0800

    Create CreateStorefrontForm.tsx

---
+++ components/web3/CreateStorefrontForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function CreateStorefrontForm() {
+  return (
+    <form>
+      <h3>Create Storefront</h3>
+      <input type="text" placeholder="Storefront Name" />
+      <button type="submit">Create</button>
+    </form>
+  );
+}

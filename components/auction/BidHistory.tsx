2fd0c7b43fa3cc943531308b3cddbad8d4a8fe9f
bsepehri-hue <bsepehri@gmail.com>
2025-12-04 13:42:00 -0800

    Update BidHistory.tsx

--- components/auction/BidHistory.tsx
+++ components/auction/BidHistory.tsx
@@ -1,8 +1,11 @@
+// components/auction/BidHistory.tsx
+"use client";
+
 export default function BidHistory() {
   return (
-    <div>
-      <h3>Bid History</h3>
-      <ul>
+    <div className="p-4 border rounded">
+      <h3 className="font-semibold mb-2">Bid History</h3>
+      <ul className="list-disc pl-5 text-gray-700">
         <li>No bids yet</li>
       </ul>
     </div>

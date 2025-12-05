fb01bb06c99f7dbdfe17dd56b3899120a17fd1ac
bsepehri-hue <bsepehri@gmail.com>
2025-12-05 14:08:22 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import WagmiClientProvider from './providers/WagmiClientProvider'; // client-only wrapper
-import TopNav from '@/components/ui/TopNav';
+import TopNav from '../components/ui/TopNav';
 
 const inter = Inter({ subsets: ['latin'] });
 

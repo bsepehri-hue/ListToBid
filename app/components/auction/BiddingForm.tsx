751e7f1190a3a1f82c794a508b130c3b47b752ea
bsepehri-hue <bsepehri@gmail.com>
2025-12-05 16:45:30 -0800

    Create auction/BiddingForm.tsx

---
+++ app/components/auction/BiddingForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function BiddingForm() {
+  return <form>Bidding form stub</form>;
+}
+
+export function BidHistory() {
+  return <div>Bid history stub</div>;
+}

c92d7f68f04aeb5ab874f1f0b0e2b40238e4b21b
bsepehri-hue <bsepehri@gmail.com>
2025-12-05 21:16:42 -0800

    Create links.ts

---
+++ lib/links/links.ts
@@ -0,0 +1,10 @@
+// lib/links.ts
+
+export const links = {
+  home: "/",
+  auctions: "/auctions",
+  dashboard: "/dashboard",
+  login: "/login",
+  profile: "/profile",
+  settings: "/settings",
+};

472c03afc7837f38b7ccf989a5cb744c66f9e0f7
bsepehri-hue <bsepehri@gmail.com>
2025-12-05 19:27:22 -0800

    Create chat/ConversationList.tsx

---
+++ app/components/chat/ConversationList.tsx
@@ -0,0 +1,14 @@
+// app/components/chat/ConversationList.tsx
+import React from "react";
+
+export const ConversationList: React.FC = () => {
+  return (
+    <div>
+      <h3>Conversation List (stub)</h3>
+      <ul>
+        <li>Conversation A</li>
+        <li>Conversation B</li>
+      </ul>
+    </div>
+  );
+};

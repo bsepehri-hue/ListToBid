c51a804c6f6e8dff2ecf2dc5a024928c0ad2f9c8
bsepehri-hue <bsepehri@gmail.com>
2025-12-05 16:49:35 -0800

    Create referral/ReferralLinkGenerator.tsx

---
+++ app/components/referral/ReferralLinkGenerator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function ReferralLinkGenerator() {
+  return <div>Referral link stub</div>;
+}
+
+export function ReferralStatsCards() {
+  return <div>Referral stats stub</div>;
+}
+
+export function ReferralActivityTable() {
+  return <div>Referral activity table stub</div>;
+}

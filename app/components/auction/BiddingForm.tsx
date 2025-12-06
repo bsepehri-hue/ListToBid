8eee49a2de6f55e35cced49d31637bb3b639be50
bsepehri-hue <bsepehri@gmail.com>
2025-12-06 15:36:06 -0800

    Delete app/components/auction/BiddingForm.tsx

--- app/components/auction/BiddingForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function BiddingForm() {
-  return <form>Bidding form stub</form>;
-}
-
-export function BidHistory() {
-  return <div>Bid history stub</div>;
-}

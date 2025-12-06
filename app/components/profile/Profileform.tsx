028d2edaac73cfbe5f6c79db411353d3fe6a1188
bsepehri-hue <bsepehri@gmail.com>
2025-12-05 16:55:09 -0800

    Create profile/Profileform.tsx

---
+++ app/components/profile/Profileform.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function ProfileForm() {
+  return <form>Profile form stub</form>;
+}
+
+export function ActivityList() {
+  return <ul><li>Activity stub</li></ul>;
+}

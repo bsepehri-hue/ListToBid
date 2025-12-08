c352ba71dbd0eb94467588c69074b8c34e43767b
bsepehri-hue <bsepehri@gmail.com>
2025-12-07 19:31:27 -0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -54,3 +54,27 @@ export const formatDuration = (ms: number): string => {
 
   return parts.join(" ");
 };
+
+/**
+ * Generates a shareable URL for an auction listing, optionally including a referral address.
+ * @param auctionId The ID of the auction.
+ * @param refAddress The optional Ethereum address of the referrer.
+ * @returns The complete, shareable URL string.
+ */
+export function generateShareableAuctionLink(
+  auctionId: string | bigint,
+  refAddress?: string
+): string {
+  const baseUrl =
+    typeof window !== "undefined"
+      ? `${window.location.origin}/auctions/${auctionId}`
+      : `https://listtobid.com/auctions/${auctionId}`;
+
+  const url = new URL(baseUrl);
+
+  if (refAddress) {
+    url.searchParams.set("ref", refAddress);
+  }
+
+  return url.toString();
+}

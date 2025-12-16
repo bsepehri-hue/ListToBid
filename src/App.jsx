9ae2aacc2cbac1ca2348a7a2f3c3a6bbc6a68e81
bsepehri-hue <bsepehri@gmail.com>
2025-12-16 06:06:09 -0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* other components like ProductCard, CartSidebar */}
+    </div>
+  );
+}
+
+export default App;

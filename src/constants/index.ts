907c1da03091547bab73d140c1a36ab78b211914
bsepehri-hue <bsepehri@gmail.com>
2025-12-19 00:36:32 -0800

    Create constants/index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+// src/constants/index.ts
+export const FREQUENCIES = [
+  { value: "daily", label: "Daily" },
+  { value: "weekly", label: "Weekly" },
+  { value: "monthly", label: "Monthly" },
+];

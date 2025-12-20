487231f625020f33bffe498e7e45bd0b11f9bc56
bsepehri-hue <bsepehri@gmail.com>
2025-12-20 04:21:25 -0800

    Update QueryProvider.tsx

--- app/providers/QueryProvider.tsx
+++ app/providers/QueryProvider.tsx
@@ -5,6 +5,5 @@ import { ReactNode, useState } from "react";
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }

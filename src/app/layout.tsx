f06d33820ac29c57562e2ff3202b9140687542d4
bsepehri-hue <bsepehri@gmail.com>
2025-12-19 20:45:08 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import Web3Provider from "@/components/providers/Web3Provider";
+import { WalletProvider } from "@/context/WalletContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Web3Provider>{children}</Web3Provider>
+        <Web3Provider>
+          <WalletProvider>
+            {children}
+          </WalletProvider>
+        </Web3Provider>
       </body>
     </html>
   );

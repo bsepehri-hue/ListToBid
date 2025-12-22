2c824468f55e65fdcc3468c9f457c54c83ea4400
bsepehri-hue <bsepehri@gmail.com>
2025-12-22 00:40:24 -0800

    Update TopNav.tsx

--- src/components/ui/TopNav.tsx
+++ src/components/ui/TopNav.tsx
@@ -63,7 +63,19 @@ const [userMenuOpen, setUserMenuOpen] = useState(false);
   }, []);
 
   return (
-    <nav className="l2b-nav l2b-flex-between l2b-items-center l2b-px-6 l2b-py-4 l2b-shadow l2b-bg-surface">
+    <nav className="
+  l2b-nav
+  l2b-flex-between
+  l2b-items-center
+  l2b-px-6
+  l2b-py-4
+  l2b-shadow
+  l2b-bg-surface
+  l2b-sticky
+  l2b-top-0
+  l2b-z-50
+  l2b-backdrop-blur
+">
 
       {/* LEFT SIDE */}
       <div className="l2b-flex l2b-items-center l2b-gap-6 l2b-hidden md:l2b-flex">

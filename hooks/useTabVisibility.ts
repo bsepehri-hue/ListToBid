695d0e20aa9268faaf90b6d5dcc2e8675736ff50
bsepehri-hue <bsepehri@gmail.com>
2025-12-22 04:35:07 -0800

    Update useTabVisibility.ts

--- hooks/useTabVisibility.ts
+++ hooks/useTabVisibility.ts
@@ -1,1 +1,17 @@
-useTabVisibility.ts
+import { useEffect, useState } from "react";
+
+export function useTabVisibility() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    function handleVisibility() {
+      setIsVisible(!document.hidden);
+    }
+
+    document.addEventListener("visibilitychange", handleVisibility);
+    return () =>
+      document.removeEventListener("visibilitychange", handleVisibility);
+  }, []);
+
+  return { isVisible };
+}

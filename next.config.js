cee06aba8cc996c26f8d606073198da3f5cd6e71
bsepehri-hue <bsepehri@gmail.com>
2025-12-23 09:28:04 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+};  // ← this is the only } you need
 
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+export default nextConfig;

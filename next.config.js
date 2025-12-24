237647ebc6787f07b9ec31edc6bb6e7425973260
bsepehri-hue <bsepehri@gmail.com>
2025-12-24 02:59:35 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.watchOptions = {
-      ignored: [
-        '**/node_modules/**',
-        '/data/**',
-        '/data/data/**',
-        '/'
-      ],
-    };
-    return config;
-  },
 };
 
 export default nextConfig;

85f0f2679beb3868c534bd43517c5a866eada7fa
bsepehri-hue <bsepehri@gmail.com>
2025-12-24 20:02:07 -0800

    Update page.tsx

--- app/(dashboard-group)/page.tsx
+++ app/(dashboard-group)/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import { Zap } from "lucide-react";
-
+import TestClient from "./TestClient";
 import TimelineFetcher from "./TimelineFetcher";
 import { mockAuctionList } from "@/auctions/mockData";
 import BidChart from "@/auctions/BidChart";

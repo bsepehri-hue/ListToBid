f9e71b50bfc3bb23782d18e13ea90211b4fe9201
bsepehri-hue <bsepehri@gmail.com>
2025-12-25 02:39:06 -0800

    Update firebase.ts

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -5,6 +5,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "stub-api-key",
@@ -12,5 +13,6 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "stub-project-id",
 };
 
+export const storage = getStorage(app);
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);

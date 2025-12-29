4837177befa6c6ba6955aaecefbce7a3545bf179
bsepehri-hue <bsepehri@gmail.com>
2025-12-29 02:42:17 -0800

    Update page.tsx

--- components/listings/categories/homes/page.tsx
+++ components/listings/categories/homes/page.tsx
@@ -1,10 +1,10 @@
 import BaseListingForm from "@/components/listings/BaseListingForm";
-import CarsForm from "@/components/listings/categories/CarsForm";
+import HomesForm from "@/components/listings/categories/HomesForm";
 
-export default function NewCarListingPage() {
+export default function NewHomeListingPage() {
   return (
-    <BaseListingForm category="cars">
-      <CarsForm />
+    <BaseListingForm category="homes">
+      <HomesForm />
     </BaseListingForm>
   );
 }

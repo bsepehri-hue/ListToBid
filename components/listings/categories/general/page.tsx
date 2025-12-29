4e3b3ea1cba88612c3827322c0019645320ab8b9
bsepehri-hue <bsepehri@gmail.com>
2025-12-29 02:57:38 -0800

    Update page.tsx

--- components/listings/categories/general/page.tsx
+++ components/listings/categories/general/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import BaseListingForm from "@/components/listings/BaseListingForm";
 import GeneralForm from "@/components/listings/categories/GeneralForm";
 
@@ -8,3 +10,19 @@ export default function NewGeneralListingPage() {
     </BaseListingForm>
   );
 }
+
+
+
+export default function GeneralForm({ extraFields, setExtraFields }) {
+  return (
+    <div className="space-y-6">
+
+      <input
+        placeholder="Brand"
+        className="w-full px-4 py-2 border rounded-lg"
+        value={extraFields.brand || ""}
+        onChange={(e) => setExtraFields({ ...extraFields, brand: e.target.value })}
+      />
+
+      <input
+        placeholder="Model"

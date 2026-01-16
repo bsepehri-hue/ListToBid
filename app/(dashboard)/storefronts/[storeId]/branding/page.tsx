dd5f77ee90ac839f62f77b477fc6e260ba425d9a
bsepehri-hue <bsepehri@gmail.com>
2026-01-16 00:27:13 -0800

    Update page.tsx

--- app/(dashboard)/storefronts/[storeId]/branding/page.tsx
+++ app/(dashboard)/storefronts/[storeId]/branding/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from '../../../../../lib/firebase';
 import UploadListingImages from '../../../../../app/components/UploadListingImages';
 
 export default function StorefrontBrandingPage() {

67b7042d731e71aebf862796d1456a219273320a
bsepehri-hue <bsepehri@gmail.com>
2026-01-15 23:36:57 -0800

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from '../../components/layout/Sidebar';
 import Link from "next/link";
-import TopLoader from '../../components/TopLoader';
+import TopLoader from '../../app/components/TopLoader';
 import { ToastProvider } from '../../context/ToastContext';
 import CommandPalette from '../../components/CommandPalette';
 import PageTransition from '../../components/PageTransition';

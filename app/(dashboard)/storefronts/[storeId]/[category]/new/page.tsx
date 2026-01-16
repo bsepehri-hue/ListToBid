694cc8c20b82830233d340974e0ae5a6e8ca76a5
bsepehri-hue <bsepehri@gmail.com>
2026-01-16 00:25:51 -0800

    Update page.tsx

--- app/(dashboard)/storefronts/[storeId]/[category]/new/page.tsx
+++ app/(dashboard)/storefronts/[storeId]/[category]/new/page.tsx
@@ -3,9 +3,8 @@
 import { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from '../../../../../lib/firebase';
 import UploadListingImages from '../../../../../app/components/UploadListingImages';
-
 export default function AddGeneralListingPage() {
   const { storeId, category } = useParams();
   const router = useRouter();

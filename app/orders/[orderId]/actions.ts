c43505ea33b805af56483271809f96ebf946ef0a
bsepehri-hue <bsepehri@gmail.com>
2026-01-20 01:07:56 -0800

    Update actions.ts

--- app/orders/[orderId]/actions.ts
+++ app/orders/[orderId]/actions.ts
@@ -1,5 +1,15 @@
 "use server";
 
+export async function markAsShipped(formData: FormData) {
+  const orderId = formData.get("orderId");
+  console.log("markAsShipped", orderId);
+}
+
+export async function markAsDelivered(formData: FormData) {
+  const orderId = formData.get("orderId");
+  console.log("markAsDelivered", orderId);
+}
+
 export async function markAsCompleted(formData: FormData) {
   const orderId = formData.get("orderId");
   console.log("markAsCompleted", orderId);

e4957eb4b7ff1428a94cf3388aa9e1a80fd96d2a
bsepehri-hue <bsepehri@gmail.com>
2026-01-27 20:02:51 -0800

    Create page.tsx

---
+++ app/storefront/[storefrontId]/page.tsx
@@ -0,0 +1,33 @@
+import { StorefrontBanner } from "./StorefrontBanner";
+import { StorefrontListings } from "./StorefrontListings";
+import { getStorefrontData } from "./getStorefrontData"; // whatever your fetch layer is
+
+export default async function StorefrontPage({
+  params,
+}: {
+  params: { storefrontId: string };
+}) {
+  const storefrontId = params.storefrontId;
+
+  // Fetch storefront + listings
+  const data = await getStorefrontData(storefrontId);
+
+  if (!data) {
+    return (
+      <div className="text-center text-gray-500 py-20">
+        Storefront not found
+      </div>
+    );
+  }
+
+  return (
+    <div className="pb-10">
+      <StorefrontBanner storefrontId={storefrontId} />
+
+      <div className="px-6">
+        <StorefrontListings listings={data.listings} />
+      </div>
+    </div>
+  );
+}
+

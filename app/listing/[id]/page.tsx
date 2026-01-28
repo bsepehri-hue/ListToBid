3427d5f10028946ca575f390d76f3ca542d46edd
bsepehri-hue <bsepehri@gmail.com>
2026-01-27 20:06:41 -0800

    Add ListingDetailPage component for listing details

---
+++ app/listing/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { getListingById } from "./getListingById";
+import { ShareModal } from "@/components/modals/ShareModal";
+import { ContactModal } from "@/components/modals/ContactModal";
+import { RatingStars } from "@/components/common/RatingStars";
+import { useState } from "react";
+
+export default async function ListingDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const listingId = params.id;
+  const listing = await getListingById(listingId);
+
+  if (!listing) {
+    return (
+      <div className="text-center text-gray-500 py-20">
+        Listing not found
+      </div>
+    );
+  }
+
+  return (
+    <div className="pb-20">
+      <ListingDetailClient listing={listing} />
+    </div>
+  );
+}

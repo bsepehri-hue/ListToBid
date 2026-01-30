9fd78c35ec7e4b025b66a7efc5f0044c1d83ffae
bsepehri-hue <bsepehri@gmail.com>
2026-01-29 21:32:19 -0800

    Update page.tsx

--- app/listing/[id]/page.tsx
+++ app/listing/[id]/page.tsx
@@ -1,25 +1,7 @@
-import { getListingById } from "./getListingById";
-import { ShareModal } from "@/components/modals/ShareModal";
-import { ContactModal } from "@/components/modals/ContactModal";
-import { RatingStars } from "@/components/common/RatingStars";
-import { useState } from "react";
 import { ListingDetailClient } from "./ListingDetailClient";
 
-export default async function ListingDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const listingId = params.id;
-  const listing = await getListingById(listingId);
-
-  if (!listing) {
-    return (
-      <div className="text-center text-gray-500 py-20">
-        Listing not found
-      </div>
-    );
-  }
+export default async function Page({ params }) {
+  const listing = await getListing(params.id);
 
   return (
     <div className="pb-20">

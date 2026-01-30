74a73f231a74c6c81661e7adce1ee7b676e4c1ff
bsepehri-hue <bsepehri@gmail.com>
2026-01-29 21:02:49 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import "./globals.css";
 import { Providers } from "./providers";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className="light" style={{ colorScheme: "light" }}>
       <body className="font-sans bg-slate-950 text-slate-50">

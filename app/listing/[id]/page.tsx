693bc7f325affc587533b71435ef1c0dca71d1ec
bsepehri-hue <bsepehri@gmail.com>
2026-01-29 21:26:27 -0800

    Update page.tsx

--- app/listing/[id]/page.tsx
+++ app/listing/[id]/page.tsx
@@ -3,7 +3,7 @@ import { ShareModal } from "@/components/modals/ShareModal";
 import { ContactModal } from "@/components/modals/ContactModal";
 import { RatingStars } from "@/components/common/RatingStars";
 import { useState } from "react";
-import ListingDetailClient from "@/app/components/ListingDetailClient";
+import { ListingDetailClient } from "./ListingDetailClient";
 
 export default async function ListingDetailPage({
   params,
